fc0d17a674c0cb5ea5da815b9c535192fb0af61a
Aditya Chopra <achopra1999@gmail.com>
2020-08-06 21:03:36 +0530

    Add files via upload

---
+++ src/st.js
@@ -0,0 +1,28 @@
+import Plane from "./design/plane.png"
+const abc={
+   "0":{
+      "Plane":Plane,
+      "fcol":"#326442",
+   },
+   "1":{
+      "Plane":Plane,
+      "fcol":"#8B252D",
+      
+   },
+   "2":{
+      "Plane":Plane,
+      "fcol":"#952F00",
+      
+   },
+   "3":{
+      "Plane":Plane,
+      "fcol":"#C09B3D",
+      
+   },
+   "4":{
+      "Plane":Plane,
+      "fcol":"#F3452C",
+      
+   }
+}
+export default abc;
